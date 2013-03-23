6ed854d0610b5ad16852a3892180375eeb7ae961
camilo <camilo_baez@gmail.com>
2013-03-23 17:31:17 -0300

    prueba

--- mierda.py
+++ mierda.py
@@ -3,6 +3,6 @@ app = Flask (__name__)
 app.debug= True
 @app.route("/")
 def hello():
-	return "hello camilo juanchi"
+	return "hello juanchi putasooooooooooooo"
 if __name__ == "__main__":
 	app.run()
