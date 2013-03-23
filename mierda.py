7aa087da18a5bd2cf3f9ba36e3f8da91a5b4357a
camilo <camilo_baez@gmail.com>
2013-03-23 18:02:37 -0300

    oiwueroiewr

--- mierda.py
+++ mierda.py
@@ -3,10 +3,6 @@ app = Flask (__name__)
 app.debug= True
 @app.route("/")
 def hello():
-<<<<<<< HEAD
-	return "hello"
-=======
-	return "hello puta poletti"
->>>>>>> branch 'master' of https://github.com/camilobaez/EPM.git
+	return "poletti"
 if __name__ == "__main__":
 	app.run()
