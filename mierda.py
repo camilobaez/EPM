470c187e49c7a701bae8347b2650d3c6cbf662d2
admin <admin@hotmail.com>
2013-03-23 17:39:19 -0300

    efrdsf

--- mierda.py
+++ mierda.py
@@ -3,6 +3,6 @@ app = Flask (__name__)
 app.debug= True
 @app.route("/")
 def hello():
-	return "hello juanchi putasooooooooooooo"
+	return "hello camilo putasooooooooooooo"
 if __name__ == "__main__":
 	app.run()
